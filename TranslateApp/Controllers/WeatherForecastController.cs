56f6c39466a91ad7ef746876af3e1d179fe9413b
“VibithaVarghese” <vibithavarghese@gmail.com>
2022-09-22 19:01:49 -0400

    Done database

--- TranslateApp/Controllers/WeatherForecastController.cs
+++ TranslateApp/Controllers/WeatherForecastController.cs
@@ -1,36 +1,54 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Razor;
+using System.Data;
+using System.Data.SqlClient;
+using Newtonsoft.Json;
 
 namespace TranslateApp.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class ForecastController : ControllerBase
+    public class TranslateController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+        SqlConnection con = new SqlConnection("server=DESKTOP-GIE55NI; database=TranslatorTable; Uid=auth_windows");
+        private readonly ILogger<TranslateController> _logger;
 
-        private readonly ILogger<ForecastController> _logger;
-
-        public ForecastController(ILogger<ForecastController> logger)
+        public TranslateController(ILogger<TranslateController> logger)
         {
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetForecast")]
-        public String Get(int id)
+        [HttpGet(Name = "GetTranslate")]
+        public String Get(String translationId, String? language = null, String? clientID = null)
         {
-            //IEnumerable<Forecast>
-            return "100";
-                /*Enumerable.Range(1, 5).Select(index => new Forecast
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter("select * from TranslatorTable where TraslatorID = 'PermitName' AND ClientId is NULL", con);
+            DataTable dt = new DataTable();
+            sqlDataAdapter.Fill(dt);
+            if(dt.Rows.Count > 0)
             {
-                Date = DateTime.Now.AddDays(index),
-                //TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();*/
+                return "very good";
+
+            }
+
+            /*
+             * 1. When the lanuage is null then assign the default language
+             * as english.
+            */
+            if (language == null) { language = "en"; }
+
+            /* 
+             * 2. Here access the correct value from the DB with giving 
+             * translationId, language & clientID.
+             * Current database have the following fields:
+             * 
+            */
+
+
+
+
+            return translationId;
+
+
         }
        
     }
